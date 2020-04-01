4b2ca795403d5423e654ee6830931756ab31d28e
kmin9 <kmin9@LAPTOP-CRCTIDMV>
2020-04-01 22:56:25 +0900

    회원가임 MVC2 중간 업로드

--- SportCenter/src/center/main/CenterSQL2.java
+++ SportCenter/src/center/main/CenterSQL2.java
@@ -11,9 +11,9 @@ public class CenterSQL2 extends Global{
 	{
 	try {
 		Connection CN = DB.getConnection();
-		System.out.println("GuesSQL.java에 연결 성공");
+		System.out.println("CenterSQL2.java에 연결 성공");
 	} catch (Exception e) {
-		System.out.println("GuesSQL.java에 연결 실패 : " + e);
+		System.out.println("CenterSQL2.java에 연결 실패 : " + e);
 	}
 }
 	
@@ -21,21 +21,32 @@ public class CenterSQL2 extends Global{
 		int ok = 0;
 		try {
 
-		 msg="insert into guest values(?, ?, ?, sysdate, ?, 0, ?)";
+		 msg="insert into CLIENTDATA values(?,?,?,?,TO_DATE(?,'YYYYMMDD'),?,?,?)";
 		 PST = CN.prepareStatement(msg);
-		 	   PST.setInt(1, dto.getSabun()); //PST.setInt(1, a);
-		 	   PST.setString(2, dto.getName()); //PST.setString(2, b);
-		 	   PST.setString(3, dto.getTitle()); //PST.setString(3, c);
-		 	   PST.setInt(4, dto.getPay());  //PST.setInt(4, d);
-		 	   PST.setString(5, dto.getEmail()); //PST.setString(5, e);
+		 	   PST.setString(1, dto.getId());
+		 	   PST.setString(2, dto.getPwd());
+		 	   PST.setString(3, dto.getName());
+		 	   PST.setString(4, dto.getPhone());
+		 	   PST.setDate(5, dto.getBdate());
+		 	   PST.setInt(6, dto.getGender());
+		 	   PST.setString(7, dto.getEmail());
+		 	   PST.setString(8, dto.getLoc());
 		 ok = PST.executeUpdate();
-		 if(ok>0){System.out.println("----"); }
-		}catch(Exception ex){ }
+		 if(ok>0){System.out.println("CenterSQL2 centerInsert 성공"); }
+		}catch(Exception ex){System.out.println("CenterSQL2 centerInsert 실패" + ex); }
 		return ok ;
 	  }//end
-	  
-	
-	
-	
-	
+	 
+	 public int centerInsert2(CenterDTO2 dto) {
+		int ok = 0;
+		try {
+
+		 msg="insert into CLIENTDATA values('"+dto.getId()+"','"+dto.getPwd()+"','"+dto.getName()+"','"+dto.getPhone()+"',TO_DATE("+dto.getBdate()+",'YYYYMMDD'),"+dto.getGender()+",'"+dto.getEmail()+"','"+dto.getLoc()+"')";
+			ST=CN.createStatement();
+			ok=ST.executeUpdate(msg);
+		 
+		 if(ok>0){System.out.println("CenterSQL2 centerInsert 성공"); }
+		}catch(Exception ex){System.out.println("CenterSQL2 centerInsert 실패" + ex); }
+		return ok ;
+	  }//end
 }
--- SportCenter/src/center/servlet/signUpinsertController.java
+++ SportCenter/src/center/servlet/signUpinsertController.java
@@ -2,6 +2,8 @@ package center.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletContext;
@@ -14,13 +16,15 @@ import javax.swing.text.DefaultFormatterFactory;
 
 import center.main.CenterDTO2;
 import center.main.CenterSQL2;
+import oracle.sql.DATE;
+
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.oreilly.servlet.multipart.MultipartParser;
 
 
-@WebServlet("/signUp.tis")
-public class signUpController extends HttpServlet {
+@WebServlet("/signUpinsert.center")
+public class signUpinsertController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -32,13 +36,10 @@ public class signUpController extends HttpServlet {
 	}//end
 
 	public void doUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//�������� response.getWriter().append("Served at: ").append(request.getContextPath());
-		//�������� ~.append(request.getContextPath());
-		//<%@ page ~ contentType="text/html; charset=UTF-8"   pageEncoding="UTF-8"%>
-		
+
 		response.setContentType("text/html;charset=UTF-8");
 		response.setCharacterEncoding("UTF-8");
-		PrintWriter out = response.getWriter(); 
+//		PrintWriter out = response.getWriter(); 
 	
 //		파일업로드
 //		ServletContext application = this.getServletContext();
@@ -46,20 +47,28 @@ public class signUpController extends HttpServlet {
 //		MultipartRequest mr = new MultipartRequest(request, "���", 1024, "UTF-8", dfp);
 //		
 
+		
+		SimpleDateFormat fm = new SimpleDateFormat("yyyy-MM-dd");
+
 		String Gid = request.getParameter("id");
 		String Gpwd = request.getParameter("pwd");
 		String Gname = request.getParameter("name");
 		String Gphone = request.getParameter("phone");
-		Date Gbdate = request.getParameter("bdate");
+		String Change = request.getParameter("bdate");
+		Date Gbdate = null;
+		try {
+			Gbdate = fm.parse(Change);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
 		int Ggender = Integer.parseInt(request.getParameter("gender"));
 		String Gemail = request.getParameter("email");
 		String Gloc = request.getParameter("loc");
 		
 		
+		try {
 		CenterDTO2 dto = new CenterDTO2();
 		CenterSQL2 sql = new CenterSQL2();
-		//��1��������=DBó��������=�����Ͻ�����=CRUDó����
-		//dto�� setXXX(�Ű�����) => sql.guestInsert(dto)
 		dto.setId(Gid);
 		dto.setPwd(Gpwd);
 		dto.setName(Gname);
@@ -68,16 +77,15 @@ public class signUpController extends HttpServlet {
 		dto.setGender(Ggender);
 		dto.setEmail(Gemail);
 		dto.setLoc(Gloc);
-		int OK=sql.guestInsert(dto);
+		int OK=sql.centerInsert2(dto);
 		if(OK>0) { 
-			System.out.println("InsertController���� ���强��");
-			response.sendRedirect("glist.tis");
+			System.out.println("signUpinsertController 회원가입 성공");
 		}
-		out.println("<h2>");
-		out.println("<a href='index.jsp'> [index.jsp] </a>");
-		out.println("<a href='guestWrite.jsp'> [�űԵ��] </a>");
-		out.println("<a href='glist.tis'> [guestList] </a>");
-		out.println("</h2>");
+		} catch (Exception e) {
+		System.out.println("에러다 임마 : " + e);
+		}
+
+		response.sendRedirect("FirstPage.jsp");
 	}//end
 	
 }//class END
